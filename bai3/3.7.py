ba2cf0f7ea98dc1123e4fc2a22aeafb88bd12f7e
hoducminh19 <hoducminh31@gmail.com>
2024-11-28 16:32:47 +0700

    Add files via upload

---
+++ bai3/3.7.py
@@ -0,0 +1,10 @@
+print('sinh vien: ho duc minh')
+print('mssv:235752021610038')
+print('##########')
+n=int(input("nhập vào một số:"))
+def checkValue(n):
+    if n % 2 == 0:
+        print("Đây là một số chẵn")
+    else:
+        print("Đây là một số lẻ")
+checkValue(n)
